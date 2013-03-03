d305e844005affaf200e8c608c10559abd3d0e11
Michael Bauer <mihi@lo-res.org>
2013-03-03 02:58:21 +0100

    added handling of foreign keys - untested

--- src/sqloader/core.clj
+++ src/sqloader/core.clj
@@ -1,14 +1,22 @@
 (ns sqloader.core (:use '(csv-map core)))
 
+(defn column-definition 
+  "creates the definition of a single column"
+  [column]
+  (let [name (get column 0)
+        type (get (get column 1) :type)
+        foreign (get (get column 1) :foreign)]
+    (if foreign
+      (format "%s %s REFERENCES %s(%s)"
+              name type (get foreign :table) (get foreign :key))
+      (format "%s %s" name type))))
+
 (defn column-definitions 
   "create the column definition string"
   [table-map]
   (let [column-map (get table-map :column-map)]
    (clojure.string/join ", " 
-                        (map (fn [x] 
-                               (format "%s %s" 
-                                       (get x 0) 
-                                       (get (get x 1) :type)))
+                        (map column-definition
                              column-map))))
 
 (defn create-table 
